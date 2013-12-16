a8e5cd4e9781b6e2444336b83edba138428894af
Maxime Vidori <maxime.vidori@gmail.com>
2013-12-16 17:51:06 +0100

    Recursive installation working

--- install.sh
+++ install.sh
@@ -1,34 +1,53 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
 
+PROJECT="RulzUrArch"
 MODULES=("RulzUrAPI")
 TOP_DIR=$(cd $(dirname "$0") && pwd)
 
-check_virtualenv () {
-	if [[ -z "$VIRTUAL_ENV" ]]
-	then
-    echo "your are not in a dedicated virtualenv, aborting..."
-    exit 1
-	fi
+install_packages () {
+  VENV=0
+  VENV_WRAPPER=0
+
+  command -v git > /dev/null || brew install git 2> /dev/null || sudo apt-get install -y git  
+  command -v pip > /dev/null || brew install pip 2> /dev/null || sudo apt-get install -y python-pip  
+
+
+  if [[ ! $(pip freeze 2> /dev/null | grep virtualenv=) ]]
+  then
+    echo "install virtualenv"
+    pip install virtualenv > /dev/null || sudo pip install virtualenv > /dev/null
+  fi
+
+  if [[ ! $(pip freeze 2> /dev/null | grep virtualenvwrapper=) ]]
+  then
+    echo "install virtualenvwrapper"
+    pip install virtualenvwrapper > /dev/null || sudo pip install virtualenvwrapper > /dev/null
+    if [[ ! $(grep "virtualenvwrapper" "$HOME/.bashrc") ]]      
+    then
+      echo "source /usr/local/bin/virtualenvwrapper.sh" >> "$HOME/.bashrc"
+    fi    
+  fi
+  source "/usr/local/bin/virtualenvwrapper.sh"  
 }
 
-git_clone () {
-  cd "$TOP_DIR"
+repositories_initialization () {
+  set +e # mkvirtualenv fails with no reason
+  mkvirtualenv "$PROJECT"  
+  pip install -r "$TOP_DIR""/requirements.txt"  
   for MODULE in "${MODULES[@]}"
   do
+    cd "$TOP_DIR"
     git clone "https://github.com/RulzUrLife/""$MODULE"
+    cd "$MODULE"    
+    mkvirtualenv "$MODULE"
+    pip install -r "requirements.txt"
   done
 }
 
-pip_install () {
-  check_virtualenv
-  pip install -r "$TOP_DIR""/requirements.txt"
-}
-
 main () {
-  git_clone
-  pip_install
+  install_packages
+  repositories_initialization  
 }
 
 main

59a1e8d0f8239893cd10b31ca41f9e8e00eb80f7
Maxime Vidori <maxime.vidori@gmail.com>
2013-12-22 00:22:56 +0100

    Changing the installation way

--- remote_install.sh
+++ remote_install.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 set -e
 
-PROJECT="RulzUrArch"
+command -v VBoxManage > /dev/null || ( echo "VirtualBox is not installed, aborting..." && exit 1 )
+command -v vagrant > /dev/null || ( echo "Vagrant is not installed, aborting..." && exit 1 )
+command -v curl > /dev/null || ( echo "curl is not installed, aborting..." && exit 1 )
 
-command -v git > /dev/null || ( echo "git is not installed, aborting..." && exit 1 )
-command -v vagrant > /dev/null || ( echo "vagrant is not installed, aborting..." && exit 1 )
+if [ "$(ls -A .)" ]; then
+    mkdir RulzUrKitchen
+    cd RulzUrKitchen
+fi
 
-git clone "https://github.com/RulzUrLife/""$PROJECT"
-cd "$PROJECT"
-
+curl https://raw.github.com/RulzUrLife/RulzUrArch/master/Vagrantfile -o Vagrantfile
 vagrant up
-vagrant ssh -c /vagrant/install.sh
-vagrant ssh
+
+# Due to the installation of VirtualBox guest additions during the provisioning,
+# a reboot of the Vagrant box is needed.
+vagrant halt
+vagrant up
+
+# Removing the file before git clone prevent the fail on non empty directories
+vagrant ssh -c "cd /vagrant; rm Vagrantfile; git init; git remote add origin https://github.com/RulzUrLife/RulzUrArch; git fetch; git checkout -t origin/master"
